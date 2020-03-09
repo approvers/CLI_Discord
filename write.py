c1b284ebc190859a09d1508661a0d2d85ecf2bc4
loxygenk <loxygen.k@gmail.com>
2020-03-09 16:49:53 +0900

    †追加† タイピング中機能を追加

--- write.py
+++ write.py
@@ -47,49 +47,51 @@ async def sender(channel):
         channel(Discord.Channel): 最初にメッセージを送る先です
     """
     while True:
-        message = input(">>>")
-        if message == ":change":
-            channel = select_channel()
-            continue
-        emojis = list(await channel.guild.fetch_emojis())
-        if message.count(":") >= 2:
-            mess_list = list(message)
-            colons = []
-            i = 0
-            info = {"del": [], "emoji": []}
-            for charactor in message:
-                if charactor == ":":
-                    colons.append(i)
-                i += 1
+        input("\033[2mPress Enter...\033[m")
+        async with channel.typing():
+            message = input("\033[1m>>>")
+            print("\033[m", end="")
+            if message == ":change":
+                channel = select_channel()
+                continue
+            emojis = list(await channel.guild.fetch_emojis())
+            if message.count(":") >= 2:
+                mess_list = list(message)
+                colons = []
+                i = 0
+                info = {"del": [], "emoji": []}
+                for charactor in message:
+                    if charactor == ":":
+                        colons.append(i)
+                    i += 1
 
-            encount = 0
-            for raw_emoji in emojis:
-                for i in range(len(colons) - 1):
-                    if encount == 0:
-                        encount -= 1
-                        continue
-                    if raw_emoji.name == message[colons[i] + 1: colons[i + 1]]:
-                        info["del"].append([colons[i], colons[i+1]])
-                        info["emoji"].append(raw_emoji)
-                        encount = 2
-            print(info)
-            deleted = 0
-            for i in range(len(info["del"])):
-                delete_start = info["del"][i][0] - deleted
-                delete_end = info["del"][i][1] - deleted + 1
-
-                del mess_list[delete_start: delete_end]
-                mess_list.insert(info["del"][i][0] - deleted, "{}")
-                deleted += info["del"][i][1] - info["del"][i][0]
-                print(info["del"][i][0])
-            message = "".join(mess_list)
-            for i in info["emoji"]:
-                print(i)
-                print(message)
-                message = message.replace("{}", str(i), 1)
-
-        await channel.send(message)
-
+                encount = 0
+                for raw_emoji in emojis:
+                    for i in range(len(colons) - 1):
+                        if encount == 0:
+                            encount -= 1
+                            continue
+                        if raw_emoji.name == message[colons[i] + 1: colons[i + 1]]:
+                            info["del"].append([colons[i], colons[i+1]])
+                            info["emoji"].append(raw_emoji)
+                            encount = 2
+                print(info)
+                deleted = 0
+                for i in range(len(info["del"])):
+                    delete_start = info["del"][i][0] - deleted
+                    delete_end = info["del"][i][1] - deleted + 1
+    
+                    del mess_list[delete_start: delete_end]
+                    mess_list.insert(info["del"][i][0] - deleted, "{}")
+                    deleted += info["del"][i][1] - info["del"][i][0]
+                    print(info["del"][i][0])
+                message = "".join(mess_list)
+                for i in info["emoji"]:
+                    print(i)
+                    print(message)
+                    message = message.replace("{}", str(i), 1)
+    
+            await channel.send(message)
 
 def select_channel():
     """
