e0a3dce657959c2e499b48afc081faa203483e90
loxygenk <loxygen.k@gmail.com>
2020-03-10 17:49:35 +0900

    †追加† メンションができるようになりました 着実にDiscordになってきてる

--- write.py
+++ write.py
@@ -10,6 +10,22 @@ CLIENT = discord.Client()
 TOKEN = sys.argv[1]
 CONNECTING = True
 
+def replace_at(string, start, length, new_text):
+    """
+    指定した文字列の指定した範囲を、指定した文字列で置きかえる。
+    :param string: 置き換え対象の文字列
+    :param start: 置き換え開始場所
+    :param length: 置き換える長さ
+    :param new_text: 置き換え後の文字列
+
+    replace_at("ABXXXCD", 2, 3, "--")
+    >>> AB--CD
+    """
+
+    left = string[:start]
+    right = string[start + length:]
+    return left + new_text + right
+
 
 @CLIENT.event
 async def on_ready():
@@ -97,6 +113,49 @@ async def sender(channel):
                 print(i)
                 print(message)
                 message = message.replace("{}", str(i), 1)
+        
+        # --- メンションを探す
+        # 必要なデータを持ってくる
+        guild: discord.Guild = CLIENT.guilds[0]
+        members = guild.members
+        roles = guild.roles
+
+        index = -1
+        raw_mentions = []
+        # 文字列をパースする
+        while index < len(message):
+            location = message.find("@", index + 1)
+            if location == -1:
+                break
+            
+            # @からあとの部分
+            # この直後にユーザー名かロール名が来てれば
+            # メンションとして認識されるわけだ
+            mention_text = message[message.find("@", index + 1) + 1:]
+            for member in members:
+                if mention_text.find(member.display_name) == 0 or \
+                   mention_text.find(member.name) == 0:
+                    raw_mentions.append(member)
+                    break
+            for role in roles:
+                if mention_text.find(role.name) == 0:
+                    raw_mentions.append(role)
+                    break
+
+            index = location
+ 
+        for raw_mention in raw_mentions:
+            if isinstance(raw_mention, discord.Member):
+                message = message.replace("@" + raw_mention.display_name, raw_mention.mention)
+                message = message.replace("@" + raw_mention.name, raw_mention.mention)
+            elif isinstance(raw_mention, discord.Role):
+                if not raw_mention.mentionable:
+                    print("\033[38;5;208;1m役職「{}」はメンション出来ません\033[m".format(raw_mention.name))
+                    print("たぶんロールの設定で無効になってる")
+                message = message.replace("@" + raw_mention.name, raw_mention.mention)
+            
+            print("\033[38;5;120;4mメンション >> {}\033[m".format(raw_mention.name))
+        
 
         await channel.send(message)
 
